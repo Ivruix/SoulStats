e90d92e5fc2130fc7aac9bde9c3898b0a1e94c0d
Victor <thegoomba4@gmail.com>
2025-03-10 15:55:06 +0300

    Updated buttons

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,6 +3,20 @@ function initializeTheme() {
     const savedTheme = localStorage.getItem('theme') || 'dark';
     document.body.classList.remove('dark', 'light');
     document.body.classList.add(savedTheme);
+
+    // Update theme icon on page load
+    const themeIcon = document.getElementById('theme-icon');
+    if (themeIcon) {
+        if (savedTheme === 'light') {
+            themeIcon.innerHTML = `
+                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-14c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4z"/>
+            `;
+        } else {
+            themeIcon.innerHTML = `
+                <path fill-rule="nonzero" d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-2a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"/>
+             `;
+        }
+    }
 }
 
 function toggleTheme() {
@@ -15,6 +29,20 @@ function toggleTheme() {
     const url = new URL(window.location);
     url.searchParams.set('theme', newTheme);
     window.history.pushState({}, '', url);
+
+    // Update theme icon
+    const themeIcon = document.getElementById('theme-icon');
+    if (themeIcon) {
+        if (newTheme === 'light') {
+            themeIcon.innerHTML = `
+                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-14c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4z"/>
+            `;
+        } else {
+            themeIcon.innerHTML = `
+                <path fill-rule="nonzero" d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-2a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"/>
+             `;
+        }
+    }
 }
 
 // Dashboard Functions
